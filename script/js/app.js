11e7e936e9c4a13d655cfa7706f5615c3dbfc258
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2022-04-19 15:37:15 -0300

    js

---
+++ script/js/app.js
@@ -0,0 +1,16 @@
+// Script de responsividade do menu 
+
+let menuresponsivo = document.getElementById('menu-responsivo');
+let menuicone = document.getElementById('menu-icone');
+
+function checked() {
+    menuresponsivo.style.display = 'block';
+    menuicone.style.display = 'block';
+}
+
+// Cookies
+
+let cookies = document.getElementById('cookies');
+    function clicar() {
+        cookies.style.display = 'none';
+    }
